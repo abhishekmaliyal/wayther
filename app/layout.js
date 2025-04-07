4ac498bfea8ba539b0a289cf3ec74767e4815f45
abhishekmaliyal <87431983+abhishekmaliyal@users.noreply.github.com>
2025-04-07 21:34:52 +0530

    gonna KMS because of font handling in next and browser

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,18 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
+
+const transcity = localFont({
+  src: "../public/fonts/Transcity.otf",
+  display: "swap",
+  variable: "--font-transcity",
+});
+
+const faith = localFont({
+  src: "../public/fonts/Faith.otf",
+  display: "swap",
+  variable: "--font-faith",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,12 +31,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${transcity.variable} ${faith.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ function HomeContent() {
   return (
     <div className="contain w-screen h-screen flex flex-col text-black bg-white">
       <div className="top flex w-full h-[10%]">
-        <div className="w-2/3 h-full flex items-center p-10 text-5xl">wayther.</div>
+        <div className="title w-2/3 h-full flex items-center p-10 text-5xl">wayther.</div>
         <div className="searchbar w-1/3 h-full p-4">
           <SearchEngine />
         </div>
