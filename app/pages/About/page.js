5da48e7c48f609e083f5107539053c02b9c185f2
abhishekmaliyal <87431983+abhishekmaliyal@users.noreply.github.com>
2025-04-17 12:53:03 +0530

    completed about page

--- app/pages/About/page.js
+++ app/pages/About/page.js
@@ -1,38 +1,54 @@
 import { ModeToggle } from "@/components/theme/darkmode";
 import Topbar from "@/components/Topbar";
 import Link from "next/link";
-import React from "react";
+
+import { GrLinkedin, GrGithub } from "react-icons/gr";
+import { BsGlobe2 } from "react-icons/bs";
 
 export default function page() {
   return (
     <>
       <Topbar />
-      <div className="contain flex w-full h-full p-8 text-3xl">
-        <div className="info flex flex-col justify-center p-4">
-          <h2>simple weather application built for easy to read at-a-glance weather report</h2>
-          <div className="about">
+      <div className="contain flex lg:flex-row flex-col h-screen p-8 text-3xl w-screen items-start lg:mt-20">
+        <div className="info flex-col justify-center p-4 h-max w-full lg:w-1/2 lg:py-20 ">
+          <h2 className=" lg:text-5xl">
+            simple weather application built for easy to read{" "}
+            <span className="underline heading underline-offset-8 lg:text-5xl ">at-a-glance</span>{" "}
+            weather report
+          </h2>
+          <div className="about my-10">
             <div>
-              made by Abhishek Maliyal{" "}
-              <span>
-                using{" "}
-                <div className="techstack">
-                  <div className="tech">1</div>
-                  <div className="tech">2</div>
-                  <div className="tech">3</div>
-                  <div className="tech">4</div>
-                  <div className="tech">5</div>
-                </div>
-              </span>
+              <p>
+                {" "}
+                by <span className="underline underline-offset-8 title">Abhishek Maliyal</span>{" "}
+                <span>
+                  using :-{" "}
+                  <span className="techstack lg:my-10 my-5 flex flex-col">
+                    <span className="tech hover:text-4xl hover:font-bold">- NextJS</span>
+                    <span className="tech hover:text-4xl hover:font-bold">- TailwindCSS</span>
+                    <span className="tech hover:text-4xl hover:font-bold">- OpenWeatherMapAPI</span>
+                    <span className="tech hover:text-4xl hover:font-bold">- ContextAPI</span>
+                  </span>
+                </span>
+              </p>
             </div>
           </div>
         </div>
-        <div className="links flex gap-4 p-4">
-          <Link href="#">linkedin</Link>
-          <Link href="#">github</Link>
-          <Link href="#">portfolio</Link>
+        <div className="links flex lg:flex-col gap-10 lg:gap-0 p-4 w-full lg:w-1/2 h-max lg:py-20 items-center justify-center">
+          <Link href="https://www.linkedin.com/in/abhishek-maliyal-a3113b217/" className="lg:p-6 flex flex-col lg:flex-row items-center text-xl lg:text-3xl">
+            <GrLinkedin className="w-20 h-20" />
+            <p className="lg:p-10">linkedin</p>
+          </Link>
+          <Link href="https://github.com/abhishekmaliyal" className="lg:p-6  flex flex-col lg:flex-row items-center text-xl lg:text-3xl">
+            <GrGithub className="w-20 h-20" />
+            <p className="lg:p-10">github</p>
+          </Link>
+          <Link href="https://abhishekmaliyal.netlify.app/" className="lg:p-6  flex flex-col lg:flex-row items-center text-xl lg:text-3xl">
+            <BsGlobe2 className="w-20 h-20" />
+            <p className="lg:p-10">portfolio</p>
+          </Link>
         </div>
       </div>
-
       <ModeToggle />
     </>
   );
--- components/Topbar.js
+++ components/Topbar.js
@@ -5,7 +5,10 @@ export default function Topbar() {
   return (
     <>
       <div className="top flex w-full h-20 border-b-2 ">
-        <div className="title w-2/3 h-full flex items-center p-8 text-5xl">wayther.</div>
+        <div className="title w-2/3 h-full flex items-center p-8 text-5xl">
+        
+        <Link href="/">wayther.</Link>
+        </div>
         <div className="searchbar w-1/3 h-full flex p-4 justify-end text-3xl text-gray-400">
           <Link href="/pages/About" className="px-4 flex items-center justify-center">
             <p>?</p>
